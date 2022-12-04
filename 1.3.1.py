9b6dbd791b12d789be4a141e51b2b27fd182968e
Кирилл Тарасов <kirilltarasov@MacBook-Air-Kirill.local>
2022-12-05 00:33:30 +0300

    first commit

---
+++ 1.3.1.py
@@ -0,0 +1,18 @@
+from random import randint
+
+number = int(input('Введите размер списка '))
+list = []
+list2 = []
+
+for i in range(number):
+    list.append(randint(0, 9))
+
+for i in range(len(list)):
+    while i < len(list)/2 and number > len(list)/2:
+        number = number - 1
+        a = list[i] * list[number]
+        list2.append(a)
+        i += 1
+
+print(list)
+print(list2)
